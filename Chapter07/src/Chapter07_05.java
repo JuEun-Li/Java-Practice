dbf89a5343cb2965c3c3a79fffbce67a8b40568b
JuEun-Li <59527078+JuEun-Li@users.noreply.github.com>
2020-02-27 23:19:33 +0900

    Chapter07_05

---
+++ Chapter07/src/Chapter07_05.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+//해시맵을 이용하여 단어 쌍 검색
+public class Chapter07_05 {
+
+	public static void main(String[] args) {
+		HashMap<String, String> dic = new HashMap<String, String>();
+
+		// 3개의 쌍 저장
+		dic.put("baby", "아기");
+		dic.put("love", "사랑");
+		dic.put("apple", "사과");
+
+		Scanner scanner = new Scanner(System.in);
+		while (true) {
+			System.out.println("찾고 싶은 단어는?>> ");
+			String eng = scanner.next();
+			if (eng.equals("exit")) {
+				System.out.println("종료합니다");
+				break;
+			}
+
+			String kor = dic.get(eng); //get메소드를 이용하여 탐색
+			if (kor == null)
+				System.out.println(eng + "는 없는 단어 입니다.");
+			else
+				System.out.println(kor);
+		}
+		scanner.close();
+	}
+
+}
