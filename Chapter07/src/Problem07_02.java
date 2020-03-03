99fdf8ecaeb5582875248b68b9356b8e35717a14
JuEun-Li <59527078+JuEun-Li@users.noreply.github.com>
2020-03-04 00:41:07 +0900

    Problem07_02_re

--- Chapter07/src/Problem07_02.java
+++ Chapter07/src/Problem07_02.java
@@ -4,37 +4,35 @@ import java.util.Scanner;
 public class Problem07_02 {
 
 	public static void main(String[] args) {
-		ArrayList<Character> stack = new ArrayList<Character>();
-		Scanner sc = new Scanner(System.in);
-		System.out.print("6개의 학점을 빈 칸으로 분리 입력(A/B/C/D/F) >> ");
-		for (int i = 0; i < 6; i++) {
-			char c = sc.next().charAt(0);
-			stack.add(c);
-		}
-		int sum = 0;
-		for (int i = 0; i < stack.size(); i++) {
-			char c = stack.get(i);
-			switch (c) {
-			case 'A':
-				sum += 4;
-				break;
-			case 'B':
-				sum += 3;
-				break;
-			case 'C':
-				sum += 2;
-				break;
-			case 'D':
-				sum += 1;
-				break;
-			case 'F':
-				sum += 0;
-				break;
-			}
-		}
-		double avr = (double) sum / stack.size();
-		System.out.println(avr);
-		sc.close();
-	}
+		ArrayList<String> ar = new ArrayList<String>();
+		Scanner scanner = new Scanner(System.in);
 
+		System.out.println("6개의 학점을 빈 칸으로 분리 입력(A/B/C/D/F) >> ");
+		for (int i = 0; i < 6; i++) {
+			String score = scanner.next();
+			ar.add(score);
+		} // arraylist에 학점 삽입 완료
+		
+		double sum = 0;
+
+		for (int i = 0; i < ar.size(); i++) {
+			String score = ar.get(i); // 인텍스 i번째 score값을 탐색한다.
+			
+			if (score.equals("A")) {
+				sum = sum + 4.0;
+			} else if (score.equals("B")) {
+				sum = sum + 3.0;
+			} else if (score.equals("C")) {
+				sum = sum + 2.0;
+			} else if (score.equals("D")) {
+				sum = sum + 1.0;
+			} else if (score.equals("F")) {
+				sum = sum + 0;
+			} 
+		}
+		double avr = sum / ar.size();
+		System.out.println("평균은: " + avr);
+		
+		scanner.close();
+	}
 }
