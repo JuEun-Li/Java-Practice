6b3c235af8fe2a658f8fc7f337823f5aa9b0b500
JuEun-Li <59527078+JuEun-Li@users.noreply.github.com>
2020-03-18 23:40:05 +0900

    Baekjoon_03_2741

---
+++ Baekjoon/src/Baekjoon_03_2741.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Baekjoon_03_2741 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int T = scanner.nextInt();
+
+		for (int i = 1; i <= T; i++) {
+			System.out.println(i);
+		}
+		scanner.close();
+	}
+
+}
