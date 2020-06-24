14e27aac59981251632febde2b953de0c2687a36
JuEun-Li <wndms61@gmail.com>
2020-06-25 00:29:50 +0900

    .

--- Problem07/src/Problem07_23.java
+++ Problem07/src/Problem07_23.java
@@ -2,6 +2,13 @@ import java.util.Scanner;
 
 // 배열 a의 요소 중에서 값이 x인 모든 요소의 인덱스를 앞에서부터 순서대로 저장해서 반환하는 arraySrchIdx메소드를 작성하자.
 public class Problem07_23 {
+	static int arraySrchIdx(int[] a, int x) {
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] == x)
+				return i;
+		}
+		return;
+	}
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
