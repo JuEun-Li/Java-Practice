701cfde18cb5946a36cd97e2255b7da1908b1ea2
JuEun-Li <wndms61@gmail.com>
2020-06-30 22:32:23 +0900

    .

--- Problem07/src/Problem07_24.java
+++ Problem07/src/Problem07_24.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
 
+// 배열 a에서 요소 a[idx]를 삭제한 배열을 반환하는 arrayRmvOf를 작성하자.
 public class Problem07_24 {
-
+	static void arrayRmvOf(int[] a, int del) {
+		
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("요소 수: ");
+		int n = scanner.nextInt();
+		int[] a = new int[n];
 
+		for (int i = 0; i < n; i++) {
+			System.out.print("a[" + i + "] = ");
+			a[i] = scanner.nextInt();
+		}
+		
+		System.out.print("삭제할 요소의 인덱스: ");
+		int del = scanner.nextInt();
 	}
 
 }
