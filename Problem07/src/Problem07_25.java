de99f14cfe597303d1cab575fb8fa39146f739c6
JuEun-Li <wndms61@gmail.com>
2020-07-08 23:53:16 +0900

    .

--- Problem07/src/Problem07_25.java
+++ Problem07/src/Problem07_25.java
@@ -2,11 +2,24 @@ import java.util.Scanner;
 
 // 배열 a에서 요소 a[n]부터 n개의 요소를 삭제하는 메소드를 작성하자. 
 public class Problem07_25 {
-	static int[] arrayRmvOfN(int[]a, int del_st, int del_end) {
-		for(int i = 0; i < a.length; i++) {
-			
+	static int[] arrayRmvOfN(int[] a, int del_st, int del_end) {
+		int count = 0;
+
+		for (int i = del_st; i < del_end; i++) {
+			a[i] = (Integer) null;
+		}
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] != (Integer) null)
+				count = count + 1;
+		}
+		int[] b = new int[count];
+
+		for (int i = 0; i < b.length; i++) {
+			if (a[i] != (Integer) null)
+				b[i] = a[i];
 		}
 	}
+
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("요소 수: ");
