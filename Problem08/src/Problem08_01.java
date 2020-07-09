64154fc4a539442820218244d11f7ed3000751d7
JuEun-Li <wndms61@gmail.com>
2020-07-10 00:49:14 +0900

    클래스 생성 기초

--- Problem08/src/Problem08_01.java
+++ Problem08/src/Problem08_01.java
@@ -10,8 +10,23 @@ public class Problem08_01 {
 	public static void main(String[] args) {
 		Human gildong = new Human();
 		Human chulsu = new Human();
-		
-		
+
+		gildong.name = "길동";
+		gildong.height = 180;
+		gildong.weight = 70;
+
+		chulsu.name = "철수";
+		chulsu.height = 180;
+		chulsu.weight = 72;
+
+		System.out.println("이름: " + gildong.name);
+		System.out.println("키: " + gildong.height + "cm");
+		System.out.println("몸무게: " + gildong.weight + "kg");
+
+		System.out.println("이름: " + chulsu.name);
+		System.out.println("키: " + chulsu.height + "cm");
+		System.out.println("몸무게: " + chulsu.weight + "kg");
+
 	}
 
 }
