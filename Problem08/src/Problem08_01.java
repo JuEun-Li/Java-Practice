2db2d2d70e9cd3bafd599651a2b6c36adef304f9
JuEun-Li <wndms61@gmail.com>
2020-07-09 22:49:48 +0900

    .

---
+++ Problem08/src/Problem08_01.java
@@ -0,0 +1,17 @@
+// 클래스 생성 기초
+class Human {
+	String name;
+	int height;
+	int weight;
+}
+
+public class Problem08_01 {
+
+	public static void main(String[] args) {
+		Human gildong = new Human();
+		Human chulsu = new Human();
+		
+		
+	}
+
+}
