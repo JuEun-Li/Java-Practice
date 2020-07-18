138c0d0d355d160ab0660328475c454b14c9012a
JuEun-Li <wndms61@gmail.com>
2020-07-18 22:55:32 +0900

    .

--- Problem08/src/Problem08_03.java
+++ Problem08/src/Problem08_03.java
@@ -1,9 +1,20 @@
-
+// 자동차 클래스 Car을 생성하자.
+class Car {
+	private String name;
+	private String num;
+	private int width; // 폭
+	private int height; // 높이
+	private int length; // 길이
+	private double x; // x 좌표
+	private double y; // y 좌표
+	private double tank; // 탱크 용량
+	private double fuel; // 남은 연료
+	private double sfc; // 연비
+}
 public class Problem08_03 {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
