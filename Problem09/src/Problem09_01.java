62026435d2857553ce1e0bd9012f1e7c41ae387b
JuEun-Li <wndms61@gmail.com>
2020-07-24 01:30:27 +0900

    얕은 복사

--- Problem09/src/Problem09_01.java
+++ Problem09/src/Problem09_01.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 // X 좌표와 Y 좌료로 구성된 2차원 좌표 Coordiante 클래스 작성하기.
 class Coordinate {
 	private double x;
@@ -23,14 +25,36 @@ class Coordinate {
 	void setY(double y) {
 		this.y = y;
 	} // setter
+
+	void set(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
 }
 
 public class Problem09_01 {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("좌표 p를 입력하세요: ");
+		System.out.print("x좌표: ");
+		double x = scanner.nextDouble();
+
+		System.out.print("y좌표: ");
+		double y = scanner.nextDouble();
+
+		Coordinate p = new Coordinate(x, y); // 점 p의 x, y좌표값을 저장
+		System.out.println("p = (" + p.getX() + "," + p.getY() + ")");
+		Coordinate q = p; // 점q의 좌표값으로 복사(얕은 복사)
+		
+		q.set(9.9, 9.9); // 점 q의 자표값을 변경
+		
+		System.out.println("p = (" + p.getX() + "," + p.getY() + ")");
+		System.out.println("q = (" + q.getX() + "," + p.getY() + ")");
+		
 
 	}
-//
 }
 
 // get은 필드 값을 가져오는 메소드이다.
